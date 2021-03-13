6c00186e83e719d608c66842d2a06340cd252d62
Jakub Kedzierski <248915@student.pwr.edu.pl>
2021-03-13 19:00:09 +0100

    started parsing - connection works, need futher parsing and saving to file

--- CrazyLaunchRemember/APIParser.cs
+++ CrazyLaunchRemember/APIParser.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace CrazyLaunchRemember
 {
     class APIParser
-    {
-        public void parseAPI() { }
+    { 
+
+        private const string URL = "https://lldev.thespacedevs.com/2.0.0/launch";
+    
+        public async void parseAPI() {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            HttpResponseMessage response = client.GetAsync("?format=json").Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var dataObjects =await response.Content.ReadAsStringAsync();  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                Console.WriteLine(dataObjects);
+                
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            // Make any other calls using HttpClient here.
+
+            // Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
+            client.Dispose();
+        }
+
 
     }
 }
--- CrazyLaunchRemember/CrazyLaunchRemeberApp.cs
+++ CrazyLaunchRemember/CrazyLaunchRemeberApp.cs
@@ -6,9 +6,8 @@ namespace CrazyLaunchRemember
     {
         static void Main(string[] args)
         {
-            Launch launch = new Launch();
-            launch.location = "Cape Carnavel";
-            Console.WriteLine(launch.location);
+            APIParser parser = new APIParser();
+            parser.parseAPI();
               
 
         }
