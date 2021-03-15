8824615dd739aad16ce705597d3202f4d8cbf3f4
KowalDrzo <248959@student.pwr.edu.pl>
2021-03-15 10:22:25 +0100

    Teraz będzie już dobrze na Gicie

--- CrazyRL/APIParser.cs
+++ CrazyRL/APIParser.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
 using System.Net.Http;
 using System.Net.Http.Headers;
 
-namespace CrazyLaunchRemember
+using System.Runtime.InteropServices;
+
+namespace CrazyRL
 {
     class APIParser
-    { 
-
+    {
         private const string URL = "https://lldev.thespacedevs.com/2.0.0/launch";
-    
-        public async void parseAPI() {
+
+        public async void downloadAPI()
+        {
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(URL);
 
@@ -19,9 +25,8 @@ namespace CrazyLaunchRemember
             if (response.IsSuccessStatusCode)
             {
                 // Parse the response body.
-                var dataObjects =await response.Content.ReadAsStringAsync();  //Make sure to add a reference to System.Net.Http.Formatting.dll
-                Console.WriteLine(dataObjects);
-                
+                var dataObjects = await response.Content.ReadAsStringAsync();  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                File.WriteAllText("dane_lotow.txt", dataObjects);
             }
             else
             {
@@ -32,8 +37,12 @@ namespace CrazyLaunchRemember
 
             // Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
             client.Dispose();
+
         }
 
+        public async void parseAPI()
+        {
 
+        }
     }
 }
